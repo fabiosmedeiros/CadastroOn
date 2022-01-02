7a9096d00117968875579a6cecf123957dc87057
Fabio <fabiosmedeiros@gmail.com>
2022-01-02 19:42:13 -0300

    Criação do solution e dos projetos referentes as camadas de apresentação, domínio e infraestrutura

---
+++ CadastroOn.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CadastroOn.Web.Startup))]
+namespace CadastroOn.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+### Projeto exemplo desenvolvimento de um sistema .NET com o uso de padrões
+
+- Boas práticas de programação com o uso de libs e patterns
+
+- Automapper, UnitOfWork, Repository pattern, e outros
